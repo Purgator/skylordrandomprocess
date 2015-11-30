d5ff306c39eebca1c71b31461f9279ce6e41510f
Aymeric <richard.aymeric.92@gmail.com>
2015-11-30 10:40:55 +0100

    Test

--- src/CodeCakeBuilder/Bootstrap.cmd
+++ src/CodeCakeBuilder/Bootstrap.cmd
@@ -1,1 +1,1 @@
-﻿@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+﻿&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
